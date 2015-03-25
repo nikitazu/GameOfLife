2efaa21c825fd7dc3a48ae8026b45aa973d0bc9b
Nikita B. Zuev <nikitazu@gmail.com>
2015-03-25 20:24:17 +0300

    Translating matrix

---
+++ Life.Core.Tests/TranslatingMatrixTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Life.Core.Tests
+{
+    [TestClass]
+    public class TranslatingMatrixTests
+    {
+        const int _size = 3;
+        TranslatingMatrix<int> _matrix;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _matrix = new TranslatingMatrix<int>(_size);
+        }
+
+        [TestMethod]
+        public void TestDefault()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < _size; j++)
+                {
+                    Assert.AreEqual(0, _matrix[i, j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestSetter()
+        {
+            _matrix[2, 2] = 1;
+            Assert.AreEqual(1, _matrix[2, 2]);
+        }
+
+        [TestMethod]
+        public void TestTranslateIndex()
+        {
+            _matrix[3, 3] = 1;
+            Assert.AreEqual(1, _matrix[0, 0]);
+            Assert.AreEqual(1, _matrix[3, 3]);
+        }
+    }
+}
---
+++ Life.Core/TranslatingMatrix.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life.Core
+{
+    public class TranslatingMatrix<T>
+    {
+        readonly List<List<T>> _matrix;
+        readonly int _size;
+
+        public TranslatingMatrix(int size)
+        {
+            _size = size;
+            _matrix = new List<List<T>>(size);
+            for (int i = 0; i < size; i++)
+            {
+                var row = new List<T>(size);
+                for (int j = 0; j < size; j++)
+                {
+                    row.Add(default(T));
+                }
+                _matrix.Add(row);
+            }
+        }
+
+        public T this[int x, int y]
+        {
+            get
+            {
+                return _matrix[x % _size][y % _size];
+            }
+            set
+            {
+                _matrix[x % _size][y % _size] = value;
+            }
+        }
+    }
+}
